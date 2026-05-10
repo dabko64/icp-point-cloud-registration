15da9649c4cb26ef4088e470984af81fcc3ad6ac
natalia <dabkowicznatalia@gmail.com>
2026-05-11 00:56:06 +0200

    initial ICP project structure

---
+++ icpAlgorytm.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import KDTree
+import open3d as o3d
+import time
+
+
+
+def createBoxPoints():
+    boxPoints = []
+
+
+#def createShape():
+
+
+
+#def transformPointCloud(...):
+
+
+
+#def addNoise(...):
+
+
+
+#def paintClouds(...):
+
+
+
+#def runICP(...):
+
+
+
+
+
+def main():
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
