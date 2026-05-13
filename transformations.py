7a26c8fc4b544ebf0a9178decc5837dc5636fc01
natalia <dabkowicznatalia@gmail.com>
2026-05-13 23:49:00 +0200

    add def of rotation to transformations module

--- transformations.py
+++ transformations.py
@@ -8,3 +8,13 @@ def rotation_matrix_x(angle_deg):
         [0, np.cos(angle), -np.sin(angle)],
         [0, np.sin(angle),  np.cos(angle)]
     ])
+
+
+def rotation_matrix_y(angle_deg):
+    angle = np.radians(angle_deg)
+
+    return np.array([
+        [np.cos(angle), 0, np.sin(angle)],
+        [0, 1, 0],
+        [-np.sin(angle), 0, np.cos(angle)]
+    ])
