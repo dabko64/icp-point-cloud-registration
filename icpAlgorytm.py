d66c0752c72b7a280111b4da70e5ad4fd2492be0
karolina <cieslakarolina05@gmail.com>
2026-05-17 22:23:33 +0200

    Praca nad algorytmem ICP w toku

--- icpAlgorytm.py
+++ icpAlgorytm.py
@@ -1,28 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import KDTree
-import open3d as o3d
+#import open3d as o3d
 import time
 
 
 
 def createBoxPoints():
     boxPoints = []
-
+    """Generowanie pudełka trzywymiarowego (próba)"""
+    points = []
+    for x in np.linspace(0, 1, 10):
+        for y in np.linspace(0, 1, 10):
+            points.append([x, y, 0])
+            points.append([x, y, 1])
+            points.append([0, x, y])
+            points.append([1, x, y])
+            points.append([x, 0, y])
+            points.append([x, 1, y])
+    return np.unique(points, axis=0)
 
 #def createShape():
 
 
 
-#def transformPointCloud(...):
+def transformPointCloud(points, R, t):
+    return np.dot(points, R.T) + t
+
+
+def addNoise(points, noise_level=0.02):
+    """ Szum Gaussa jako symulacja """
+    noise = np.random.normal(0, noise_level, points.shape)
 
 
 
-#def addNoise(...):
-
-
-
-#def paintClouds(...):
+def paintClouds(source, target, title="Wizualizacja chmur punktów"):
+    """ Wizualizacja chmury punktów"""
 
 
 
