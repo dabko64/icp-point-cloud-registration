7a56136bc5bf626124e7e6827cdcc4957f8b35b9
natalia <dabkowicznatalia@gmail.com>
2026-05-18 22:35:21 +0200

    started icp algorithm

--- icpAlgorytm.py
+++ icpAlgorytm.py
@@ -1,43 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import KDTree
-import open3d as o3d
+#import open3d as o3d
 import time
 
 
 
 def createBoxPoints():
-    boxPoints = []
 
+    """
+    generates points on the surface of a three-dimensional box
+    """
+
+    points = []
+    for x in np.linspace(0, 1, 10):
+        for y in np.linspace(0, 1, 10):
+            points.append([x, y, 0])
+            points.append([x, y, 1])
+            points.append([0, x, y])
+            points.append([1, x, y])
+            points.append([x, 0, y])
+            points.append([x, 1, y])
+
+    return np.unique(points, axis=0)
 
 #def createShape():
 
-
-
-#def transformPointCloud(...):
-
-
-
-#def addNoise(...):
-
-
-
-#def paintClouds(...):
-
-
-
-#def runICP(...):
-
-
-
-
-
-def main():
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
