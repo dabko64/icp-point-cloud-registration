71a971d25ef398bc21eb0249d7ea76baefc5e512
natalia <dabkowicznatalia@gmail.com>
2026-05-18 22:36:22 +0200

    start of transformations in algorithm

--- icpAlgorytm.py
+++ icpAlgorytm.py
@@ -26,3 +26,48 @@ def createBoxPoints():
 
 #def createShape():
 
+
+
+def transformPointCloud(points, R, t):
+    return np.dot(points, R.T) + t
+
+
+def addNoise(points, noise_level=0.02):
+    """gaussian noise as a simulation"""
+    noise = np.random.normal(0, noise_level, points.shape)
+    return points + noise
+
+
+
+def nearestNeighbors(source, target):
+    """
+    finds the nearest target point for every source point
+    """
+
+    tree = KDTree(source)
+    dist, ind = tree.query(target)
+
+    return dist, ind
+
+
+def bestFitTransform(source, target):
+    centroid_source = np.mean(source, axis=0)
+    centroid_target = np.mean(target, axis=0)
+
+    source_centered = source - centroid_source
+    target_centered = target - centroid_target
+
+    covariance_matrix = source_centered.T @ target_centered
+
+    U, _, Vt = np.linalg.svd(covariance_matrix)
+
+    rotation_matrix = Vt.T @ U.T
+
+    if np.linalg.det(rotation_matrix) < 0:
+        Vt[-1, :] *= -1
+        rotation_matrix = Vt.T @ U.T
+
+        translation_vector = centroid_target - rotation_matrix @ centroid_source
+
+        return rotation_matrix, translation_vector
+
